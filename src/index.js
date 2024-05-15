09bff1c1c47f1edbbae3d78b6b1fef684bf11d16
Ian <ian.mwangi1@student.moringaschool.com>
2024-05-15 22:06:52 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import "./index.css";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<Home />);
